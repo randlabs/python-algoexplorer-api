52bc7737829e6648fb49659738a14e78d9c539e6
JCibeira <jacibcas@gmail.com>
2019-12-01 15:57:42 -0400

    Config (networks)

---
+++ src/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+'''Defaults for accessing the Algoexplorer API'''
+
+MAINNET = {
+	'url': 'https://api.algoexplorer.io/v1',
+	'genesis': 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
+	'id': 'mainnet-v1.0'
+}
+
+TESTNET = {
+	'url': 'https://api.testnet.algoexplorer.io/v1',
+	'genesis': 'SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=',
+	'id': 'testnet-v1.0'
+}
+
+BETANET = {
+	'url': 'https://api.betanet.algoexplorer.io/v1',
+	'genesis': 'mFgazF+2uRS1tMiL9dsj01hJGySEmPN28B/TjjvpVW0=',
+	'id': 'betanet-v1.0'
+}

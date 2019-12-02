e735189984c6a9b9913024e02c246bef6e80977d
JCibeira <jacibcas@gmail.com>
2019-12-02 19:26:57 -0400

    Approach changed to multiple inheritance

--- src/block.py
+++ src/block.py
@@ -7,23 +7,57 @@ class Block:
 		self.config = config
 	
 	def blockCount(self):
+		'''
+        Amount of available blocks
+
+        :return: the amount of available blocks
+        :rtype: int
+
+        '''
 		response = fetchGet(self.config['url'] + '/block/count')
 		response_json = response.json()
 		return response_json['blockCount']
 
 	def queryBlock(self, hash_or_round):
+		'''
+        Block information based on the specified round or hash
+
+        :param hash_or_round: round number or hash string to query, greater or equal than 0
+        :type hash_or_round: int or str
+        :return: the block information based on the specified round or hash
+        :rtype: dict
+
+        '''
 		if (type(hash_or_round) != int and type(hash_or_round) != str) or (type(hash_or_round) == int and hash_or_round < 0):
 			return False
 		response = fetchGet(self.config['url'] + '/block/' + str(hash_or_round))
 		return response.json()
 
-	def queryLatestByRound(self, count):
+	def queryBlockLatestByRound(self, count):
+		'''
+        Latest blocks
+
+        :param count: amount of blocks to return, between 1 and 100
+		:type count: int
+        :return: the latest blocks
+        :rtype: list
+
+        '''
 		if type(count) != int or count < 1 or count > 100:
 			return False
 		response = fetchGet(self.config['url'] + '/block/latest/' + str(count))
 		return response.json()
 
-	def queryIntervalByRound(self, from_round, to_round):
+	def queryBlockIntervalByRound(self, from_round, to_round):
+		'''
+        Blocks between the specified rounds
+
+        :param int from_round: the starting round number (inclusive), greater or equal than 0
+        :param int to_round: the ending round number (inclusive), greater or equal than 1
+        :return: the blocks between the specified rounds
+        :rtype: list
+
+        '''
 		if type(from_round) != int or type(to_round) != int or to_round - from_round < 1 or from_round < 0 or to_round < 1:
 			return False
 		if to_round - from_round > 99:
@@ -31,26 +65,60 @@ class Block:
 		response = fetchGet(self.config['url'] + '/block/from/' + str(from_round) + '/to/' + str(to_round))
 		return response.json()
 
-	def queryLatestByTimestamp(self, since):
+	def queryBlockLatestByTimestamp(self, since):
+		'''
+        Latest blocks since the specified timestamp
+
+        :param int since: the earliest timestamp of the sought blocks, greater or equal than 0
+        :return: the latest blocks since the specified timestamp
+        :rtype: list
+
+        '''
 		if type(since) != int or since < 0:
 			return False
 		response = fetchGet(self.config['url'] + '/block/since/' + str(since))
 		return response.json()
 
-	def queryLatestCountByTimestamp(self, since):
+	def queryBlockLatestCountByTimestamp(self, since):
+		'''
+        Amount of blocks since the specified timestamp
+
+        :param int since: the earliest timestamp of the sought blocks, greater or equal than 0
+        :return: the amount of blocks since the specified timestamp
+        :rtype: int
+
+        '''
 		if type(since) != int or since < 0:
 			return False
 		response = fetchGet(self.config['url'] + '/block/since/' + str(since) + '/count')
 		response_json = response.json()
 		return response_json['blockCount']
 
-	def queryIntervalByTimestamp(self, since, until):
+	def queryBlockIntervalByTimestamp(self, since, until):
+		'''
+        Blocks between the specified timestamps
+
+        :param int since: the starting UTC timestamp (inclusive), greater or equal than 0
+        :param int until: the ending UTC timestamp (inclusive), greater or equal than 1
+        :return: the blocks between the specified timestamps
+        :rtype: list
+
+        '''
 		if type(since) != int or type(until) != int or until - since < 1 or until - since > 172799 or since < 0 or until < 1:
 			return False
 		response = fetchGet(self.config['url'] + '/block/since/' + str(since) + '/until/' + str(until))
 		return response.json()
 
-	def queryIntervalCountByTimestamp(self, since, until):
+	def queryBlockIntervalCountByTimestamp(self, since, until):
+		'''
+        Amount of blocks between the specified timestamps
+
+        :param int since: the starting UTC timestamp (inclusive), greater or equal than 0
+        :param int until: the ending UTC timestamp (inclusive), greater or equal than 1
+        :return: the amount of blocks between the specified timestamps
+        :rtype: int
+
+        '''
 		if type(since) != int or type(until) != int or until - since < 1 or until - since > 172799 or since < 0 or until < 1:
 			return False
 		response = fetchGet(self.config['url'] + '/block/since/' + str(since) + '/until/' + str(until) + '/count')
@@ -58,6 +126,15 @@ class Block:
 		return response_json['blockCount']
 
 	def queryBlockTransactions(self, hash_or_round):
+		'''
+        Transactions of the specified block
+
+        :param hash_or_round: round number or hash string to query, greater or equal than 0
+        :type hash_or_round: int or str
+        :return: the transactions of the specified block
+        :rtype: list
+
+        '''
 		if (type(hash_or_round) != int and type(hash_or_round) != str) or (type(hash_or_round) == int and hash_or_round < 0):
 			return False
 		response = fetchGet(self.config['url'] + '/block/' + str(hash_or_round) + '/transactions')
--- src/transaction.py
+++ src/transaction.py
@@ -7,48 +7,115 @@ class Transaction:
 		self.config = config
 	
 	def transactionsCount(self):
+		'''
+        Amount of available transactions
+
+        :return: the amount of available transactions
+        :rtype: int
+
+        '''
 		response = fetchGet(self.config['url'] + '/transaction/count')
 		response_json = response.json()
 		return response_json['txCount']
 		
 	def queryTransaction(self, txid_or_index):
+		'''
+        Transaction based on the specified index or txid
+
+        :param txid_or_index: index number or txid string to query, greater or equal than 0
+        :type txid_or_index: int or str
+        :return: the transaction based on the specified index or txid
+        :rtype: dict
+
+        '''
 		if (type(txid_or_index) != str and type(txid_or_index) != int) or txid_or_index < 0:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/' + str(txid_or_index))
 		return response.json()
 
-	def queryLatestByIndex(self, count):
+	def queryTransactionLatestByIndex(self, count):
+		'''
+        Latest transactions
+
+        :param int count: amount of transactions to return, between 1 and 100
+        :return: the latest transactions
+        :rtype: list
+
+        '''
 		if type(count) != int or count < 1 or count > 100:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/latest/' + str(count))
 		return response.json()
 
-	def queryIntervalByIndex(self, from_index, to_index):
+	def queryTransactionIntervalByIndex(self, from_index, to_index):
+		'''
+        Transactions between the specified indexes
+
+        :param int from_index: the starting index number (inclusive), greater or equal than 0
+        :param int to_index: the ending index number (inclusive), greater or equal than 1
+        :return: the transactions between the specified indexes
+        :rtype: list
+
+        '''
 		if type(from_index) != int or type(to_index) != int or to_index - from_index < 1 or from_index < 0 or to_index < 1 or to_index - from_index > 100:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/from/' + str(from_index) + '/to/' + str(to_index))
 		return response.json()
 
-	def queryLatestByTimestamp(self, since):
+	def queryTransactionLatestByTimestamp(self, since):
+		'''
+        Latest transactions since a specific timestamp.
+
+        :param int since: the earliest timestamp of the sought transactions, greater or equal than 0
+        :return: the latest transactions since a specific timestamp.
+        :rtype: list
+
+        '''
 		if type(since) != int or since < 0:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/since/' + str(since))
 		return response.json()
 
-	def queryLatestCountByTimestamp(self, since):
+	def queryTransactionLatestCountByTimestamp(self, since):
+		'''
+        Amount of transactions since the specified UTC timestamp
+
+        :param int since: the earliest timestamp of the sought transactions, greater or equal than 0
+        :return: the amount of transactions since the specified UTC timestamp
+        :rtype: int
+
+        '''
 		if type(since) != int or since < 0:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/since/' + str(since) + '/count')
 		response_json = response.json()
 		return response_json['txCount']
 
-	def queryIntervalByTimestamp(self, since, until):
+	def queryTransactionIntervalByTimestamp(self, since, until):
+		'''
+        Transactions in a date range
+
+        :param int since: the starting UTC timestamp (inclusive), greater or equal than 0
+        :param int until: the ending UTC timestamp (inclusive), greater or equal than 1
+        :return: the transactions in a date range
+        :rtype: list
+
+        '''
 		if type(since) != int or since < 0 or type(until) != int or until < 1 or until - since < 1:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/since/' + str(since) + '/until/' + str(until))
 		return response.json()
 
-	def queryIntervalCountByTimestamp(self, since, until):
+	def queryTransactionIntervalCountByTimestamp(self, since, until):
+		'''
+        Amount of transactions between the specified UTC timestamps
+
+        :param int since: the starting UTC timestamp (inclusive), greater or equal than 0
+        :param int until: the ending UTC timestamp (inclusive), greater or equal than 1
+        :return: the amount of transactions between the specified UTC timestamps
+        :rtype: int
+
+        '''
 		if type(since) != int or since < 0 or type(until) != int or until < 1 or until - since < 1:
 			return False
 		response = fetchGet(self.config['url'] + '/transaction/since/' + str(since) + '/until/' + str(until) + '/count')

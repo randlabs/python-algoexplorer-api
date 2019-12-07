af2649ac478b0a8e820c91333956a582a6d823c1
JCibeira <jacibcas@gmail.com>
2019-12-07 19:48:50 -0400

    Block model: Google Style Python Docstrings

--- src/block.py
+++ src/block.py
@@ -5,13 +5,24 @@ from typing import Union, Optional
 from request import fetchGet
 
 class Block:
-	def __init__(self, config):
+	'''
+    The Block Model
+	
+	Args:
+		config: a dictionary containing the api configuration (url, genesis, id)
+	
+    '''
+	def __init__(self, config: dict):
+		'''
+		Create a new Block with the provided attributes
+
+        '''
 		self.config = config
 	
-	def blockCount(self) -> int:
+	def queryBlockCount(self) -> int:
 		'''
-        :return: the amount of available blocks
-        :rtype: int
+		Returns:
+			the amount of available blocks
 
         '''
 		response = fetchGet(self.config['url'] + '/block/count')
@@ -20,38 +31,44 @@ class Block:
 
 	def queryBlock(self, value: Union[int, str]) -> dict:
 		'''
-        :param value: a block number or block hash
-        :return: the block information based on the specified round or hash
-        :rtype: dict
+		Args:
+			value: a block number or block hash
+		
+		Returns:
+			the block information based on the specified round or hash
 
         '''
 		if (type(value) != int and type(value) != str) or (type(value) == int and value < 0):
-			raise Exception('Invalid argument, value must be a positive integer or a string hash')
+			raise Exception('Invalid argument, VALUE must be a positive integer or a string hash')
 		response = fetchGet(self.config['url'] + '/block/' + str(value))
 		return response.json()
 
 	def queryLatestBlocks(self, count: int) -> list:
 		'''
-        :param count: amount of blocks to return. Limited to values between 1 and 100
-        :return: the latest blocks
-        :rtype: list
+		Args:
+			count: amount of blocks to return (between 1 and 100)
+
+		Returns:
+			the latest blocks
 
         '''
 		if type(count) != int or count < 1 or count > 100:
-			raise Exception('Invalid argument, count must be a positive integer between 1 and 100')
+			raise Exception('Invalid argument, COUNT must be a positive integer between 1 and 100')
 		response = fetchGet(self.config['url'] + '/block/latest/' + str(count))
 		return response.json()
 
 	def queryBlocksFromInterval(self, from_round: int, to_round: int) -> list:
 		'''
-        :param from_round: the starting round number (inclusive)
-        :param to_round: the ending round number (inclusive)
-        :return: the blocks between the specified rounds
-        :rtype: list
+		Args:
+			from_round: the starting round number (inclusive)
+			to_round: the ending round number (inclusive)
+		
+		Returns:
+			the blocks between the specified rounds
 
         '''
 		if type(from_round) != int or type(to_round) != int or to_round - from_round < 1 or from_round < 0 or to_round < 1:
-			raise Exception('Invalid arguments, from_round and to_round must be a positive integers, and to_round must be greater than from_round')
+			raise Exception('Invalid arguments, FROM_ROUND and TO_ROUND must be a positive integers, and TO_ROUND must be greater than FROM_ROUND')
 		if to_round - from_round > 99:
 			raise Exception('Max blocks to query is 100')
 		response = fetchGet(self.config['url'] + '/block/from/' + str(from_round) + '/to/' + str(to_round))
@@ -59,22 +76,26 @@ class Block:
 
 	def queryBlocksByDate(self, since: int, until: Optional[int] = None, count: Optional[bool] = False) -> Union[int, list]:
 		'''
-        :param since: the starting UTC timestamp (inclusive)
-		:param until: the ending UTC timestamp (inclusive)
-		:param count: if its true, will return the amount of blocks, else, will return an array of blocks
-        :return: the amount of blocks or a blocks list since the specified interval of time
-        :rtype: Union[int, list]
+		Args:
+			since: the starting UTC timestamp (inclusive)
+			until: the ending UTC timestamp (inclusive)
+			count: if its true, will return the amount of blocks, else, will return an array of blocks
+		
+		Returns:
+			the amount of blocks or a blocks list since the specified interval of time
 
         '''
 		date = time.time() + 60
 		if type(since) != int or (until != None and type(until) != int):
 			raise Exception('Invalid arguments, the date must be a positive integer')
-		if since < 1546300800 or since > date or (until != None and (until - since < 1 or until - since > 172799)):
+		if since < 1546300800 or since > date or (until != None and until - since < 1):
 			raise Exception('Invalid date')
 		if until != None and (until < since or until > date):
-			raise Exception('Invalid arguments, until must be greater than since')
+			raise Exception('Invalid arguments, UNTIL must be greater than SINCE')
 		if type(count) != bool:
 			raise Exception('Invalid arguments, COUNT must be a boolean')
+		if until != None and until - since > 172800:
+			raise Exception('Invalid arguments. Timestamp distance cannot exceed 172800 seconds')
 		url = self.config['url'] + '/block/since/' + str(since)
 		if count:
 			if until != None:
@@ -91,12 +112,14 @@ class Block:
 
 	def queryBlockTransactions(self, value: Union[int, str]) -> list:
 		'''
-        :param value: round number or hash string to query
-        :return: the transactions of the specified block
-        :rtype: list
+		Args:
+			value: round number or hash string to query
+		
+		Returns:
+			the transactions of the specified block
 
         '''
 		if (type(value) != int and type(value) != str) or (type(value) == int and value < 0):
-			raise Exception('Invalid argument, value must be a positive integer or a string hash')
+			raise Exception('Invalid argument, VALUE must be a positive integer or a string hash')
 		response = fetchGet(self.config['url'] + '/block/' + str(value) + '/transactions')
 		return response.json()

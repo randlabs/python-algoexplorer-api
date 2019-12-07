f8f26b1e04578a5adfaa3c5f2a808da07f1a2b9f
JCibeira <jacibcas@gmail.com>
2019-12-07 19:56:32 -0400

    Request: added typing and exception message

--- src/request.py
+++ src/request.py
@@ -3,16 +3,16 @@
 import requests
 import json
 
-def fetchGet(url):
+def fetchGet(url: str):
     headers = {'Accept': 'application/json'}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        return False
+        raise Exception('The request generates an unexpected response')
     return response
 
-def fetchPost(url, body):
+def fetchPost(url: str, body: dict):
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, data=json.dumps(body))
     if response.status_code != 200:
-        return False
+        raise Exception('The request generates an unexpected response')
     return response

4e343d6ab0dad49526c998a5c840fd84f98ee714
JCibeira <jacibcas@gmail.com>
2019-12-08 18:53:59 -0400

    Node model

---
+++ src/node.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from request import fetchGet, fetchPost
+from validates import validateHex
+
+class Node:
+	'''
+    The Node Model
+	
+	Args:
+		config: a dictionary containing the api configuration (url, genesis, id)
+	
+    '''
+	def __init__(self, config: dict):
+		'''
+		Create a new Node with the provided attributes
+
+        '''
+		self.config = config
+		
+	def sendTransaction(self, hexa: str) -> str:
+		'''
+		Args:
+		    hexa: address of the account to query
+
+		Returns:
+			information about the specified address
+
+        '''
+		if not validateHex(hexa):
+			raise Exception('Invalid argument, must be a hex string')
+		body = { 'hexa': hexa }
+		response = fetchPost(self.config['url'] + '/sendraw', body)
+		response_json = response.json()
+		return response_json['hash']
+	
+	def queryStatus(self) -> dict:
+		'''
+		Returns:
+			information about the specified address
+
+        '''
+		response = fetchGet(self.config['url'] + '/status')
+		return response.json()
